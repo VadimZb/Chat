1ff493dbd3dd4cef946abcb05b4e2bd6a4a574f6
VVZ <VVZ.dev@gmail.com>
2021-01-10 01:44:32 +0300

    good luck

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+const PORT = 5000;
+
+app.use("/static", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+let connectionCounter = 0;
+io.on("connection", (socket) => {
+    let username = "";
+
+    socket.on("name given", (username) => {
+        username = username;
+        connectionCounter++;
+        socket.broadcast.emit("user joined", username);
+        io.emit("refresh online", connectionCounter);
+
+        socket.on("disconnect", () => {
+            socket.broadcast.emit("user left", username);
+            connectionCounter--;
+            io.emit("refresh online", connectionCounter);
+        });
+    });
+
+    socket.on("message sent", (username, text) => {
+        socket.broadcast.emit("new message", username, text);
+    });
+});
+
+http.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`);
+});
